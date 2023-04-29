4eaced1a3413344ef259804eb8b2b61e29b91bc1
Rohit Dhakane <rohitdhakane6@gmail.com>
2023-04-29 20:50:15 +0530

    vercel file joined

--- client/src/helper.js
+++ client/src/helper.js
@@ -1,2 +1,2 @@
-export const BASE_URL="http://localhost:3001"
-// export const BASE_URL="https://sociopedia-backend-git-main-rohitdhakane6.vercel.app/"
+// export const BASE_URL="http://localhost:3001"
+export const BASE_URL="https://social-media-app-sociopedia-dq5lxexoz-rohitdhakane6.vercel.app"
