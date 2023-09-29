c6a4f9878b75761630a26365c60a5ee0c155d6ac
Rohit Dhakane <rohitdhakane6@gmail.com>
2023-09-29 18:05:25 +0530

    Done

--- client/src/helper.js
+++ client/src/helper.js
@@ -1,2 +1,1 @@
-// export const BASE_URL="http://localhost:3001"
-export const BASE_URL="https://social-media-app-sociopedia-dq5lxexoz-rohitdhakane6.vercel.app"
+export const BASE_URL="http://localhost:3001"
